e765edf8080c269ba1eb2a3c662584d405e9daf5
Chin Shan Hong <72982917+chinshanhong@users.noreply.github.com>
2021-06-22 18:31:38 +0800

    Create Print in Reverse.java

---
+++ Print in Reverse.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep) {
+        while (node != null) {
+            System.out.print(node.data);
+
+            node = node.next;
+
+            if (node != null) {
+                System.out.print(sep);
+            }
+        }
+    }
+    
+    
+
+    /*
+     * Complete the 'reversePrint' function below.
+     *
+     * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+     */
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+
+    public static void reversePrint(SinglyLinkedListNode llist) {
+        Stack<Integer> stack = helper(llist);
+        while(!stack.isEmpty()){
+            System.out.println(stack.pop());
+        }
+
+    }
+    public static Stack<Integer> helper(SinglyLinkedListNode llist){
+        
+        Stack<Integer> stack = new Stack<>();
+        SinglyLinkedListNode current = llist;
+        
+        while(current != null){
+            stack.push(current.data);
+            current = current.next;
+        }
+        return stack;
+    }
+}
