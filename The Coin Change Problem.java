f37d033a74060e1afdf4dfab9d90b2367adc4062
Chin Shan Hong <72982917+chinshanhong@users.noreply.github.com>
2022-02-10 15:23:29 +0800

    Create The Coin Change Problem.java

---
+++ The Coin Change Problem.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'getWays' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. LONG_INTEGER_ARRAY c
+     */
+
+    public static long getWays(int n, List<Long> c) {
+        long[] combination = new long[n + 1];
+        combination[0] = 1;
+        
+        for(int i = 0; i < c.size(); i++){
+            for(int j = 1; j < combination.length; j++){
+                if((long)j >= c.get(i)){
+                    combination[j] += combination[(int)(j - c.get(i))];
+                }
+            }
+        }
+        System.out.println(Arrays.toString(combination));
+        return combination[combination.length - 1];
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Long> c = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Long::parseLong)
+            .collect(toList());
+
+        // Print the number of ways of making change for 'n' units using coins having the values given by 'c'
+
+        long ways = Result.getWays(n, c);
+
+        bufferedWriter.write(String.valueOf(ways));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
